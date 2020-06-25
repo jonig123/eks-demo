a5928bc200a5dcdcc8d07245a2e41d0a1fdf848a
pushpendrk <pushpendra3.kumar@birlasoft.com>
2020-06-25 14:14:26 +0530

    Update project

---
+++ src/test/java/com/joni/eks/JoniEksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joni.eks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoniEksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
